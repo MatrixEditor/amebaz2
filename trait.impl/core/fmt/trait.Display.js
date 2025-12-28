164a46c00809a27fa507305f946b59bba5ab4e14
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-12-28 13:46:06 +0000

    Deploying to gh-pages from @ MatrixEditor/amebaz2@cda4a2afdc1dde06d20d9d2225686406687bd8af 🚀

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["amebazii",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"amebazii/error/enum.Error.html\" title=\"enum amebazii::error::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["amebazii",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"amebazii/error/enum.Error.html\" title=\"enum amebazii::error::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["amebazii",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"amebazii/types/enums/enum.PartitionType.html\" title=\"enum amebazii::types::enums::PartitionType\">PartitionType</a>"]]]]);
+    var implementors = Object.fromEntries([["amebazii",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"amebazii/types/enums/enum.PartitionType.html\" title=\"enum amebazii::types::enums::PartitionType\">PartitionType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
